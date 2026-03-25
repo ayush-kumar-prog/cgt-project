0eb6d751da888e659387385471ddf9c6a1d630f3
ayush-kumar-prog <ayushkumargithub@gmail.com>
2026-03-25 20:35:51 +0000

    feat: add start_simulation with historical data loading

--- game/src/leaders.py
+++ game/src/leaders.py
@@ -16,3 +16,14 @@ class AdaptiveLeader(Leader):
         self.sigma2 = 1.0
         self.P = np.eye(2) * 100.0
         self.lam = 0.97
+        self.all_uL = []
+        self.all_uF = []
+
+    def start_simulation(self):
+        for t in range(1, 101):
+            uL, uF = self.get_price_from_date(t)
+            self.hist_uL.append(uL)
+            self.hist_uF.append(uF)
+        self.all_uL = list(self.hist_uL)
+        self.all_uF = list(self.hist_uF)
+        self._fit_ols()
