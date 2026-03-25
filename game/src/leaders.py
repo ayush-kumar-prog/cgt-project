01d5e54ea4b3e822f5e4d196ade97feec674f89b
ayush-kumar-prog <ayushkumargithub@gmail.com>
2026-03-25 20:45:20 +0000

    feat: add new_price with adaptive exploration

--- game/src/leaders.py
+++ game/src/leaders.py
@@ -62,3 +62,19 @@ class AdaptiveLeader(Leader):
         self.P = (self.P - np.outer(gain, x @ self.P)) / self.lam
         self.all_uL.append(uL)
         self.all_uF.append(uF)
+
+    def new_price(self, date):
+        if date > 101:
+            prev_uL, prev_uF = self.get_price_from_date(date - 1)
+            self._rls_update(prev_uL, prev_uF)
+        if date == 101:
+            return min(5.0, self.UPPER_BOUND)
+        if date == 102:
+            prev_uL, prev_uF = self.get_price_from_date(101)
+            self._rls_update(prev_uL, prev_uF)
+            hist_mean = np.mean(self.hist_uF)
+            slope_est = (prev_uF - hist_mean) / (5.0 - np.mean(self.hist_uL))
+            if slope_est > 0.3:
+                return min(18.0, self.UPPER_BOUND)
+            self._fit_ols()
+        return self._optimal_price()
